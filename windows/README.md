dae75503afa8472d4a2c61fda6736d8b8c640bf1
theprotos <vlcz@seznam.cz>
2019-10-16 00:09:04 +0200

    added windows scripts

--- windows/README.md
+++ windows/README.md
@@ -12,10 +12,10 @@ Update below parameters in [win10workstation.json](win10workstation.json)
 ```
 "autounattend": "answer_files/win10ent/Autounattend.xml",
 "iso_checksum": "34887592ECC25B725A527748D31971F22C78C82B",
-"iso_mirror": "D:\\home\\downloads\\windows10",
-"iso_name": "windows10.iso",
+"iso_path"="D:\\home\\downloads\\windows10\\windows11.iso"
 ```
 
 ```
-packer build  -only=virtualbox-iso .\win10workstation.json
+packer build  -only=virtualbox-iso  .\win10workstation.json
+packer build  -only=virtualbox-iso -var 'iso_path=D:\\home\\downloads\\windows10\\windows11.iso' -var 'iso_checksum=34887592ECC25B725A527748D31971F22C78C82B' .\win10workstation.json
 ```
--- windows/scripts/base_setup.ps1
+++ windows/scripts/base_setup.ps1
@@ -8,11 +8,20 @@ New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowO
 
 # Does a lot: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/enable-psremoting?view=powershell-6
 Enable-PSRemoting -SkipNetworkProfileCheck -Force
+Set-NetConnectionProfile -NetworkCategory Private
+
 Set-NetFirewallRule -Name "WINRM-HTTP-In-TCP" -RemoteAddress Any # allow winrm over public profile interfaces
 
 winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
 winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="2048"}'
 winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
 winrm set "winrm/config/service/auth" '@{Basic="true"}'
+
+#Set-NetConnectionProfile -NetworkCategory Public
+
+# There needs to be Oracle CA (Certificate Authority) certificates installed in order
+# to prevent user intervention popups which will undermine a silent installation.
+certutil -addstore -f "TrustedPublisher" A:\oracle-cert.cer
+E:\VBoxWindowsAdditions.exe /S
 
 exit 0
